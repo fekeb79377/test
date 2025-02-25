610116dbbb9510007c8c6ffac11decb7a6822577
fekeb79377 <fekeb79377@outlook.com>
2025-02-25 17:42:00 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const database = require('./db/db');
-database();
+const ConnectTodatabase = require('./config/db-config');
+ConnectTodatabase();
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
